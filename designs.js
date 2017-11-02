96ce57220aa54320169841b5edcbea52574a2c22
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-02 19:44:28 +0000

    feat: Store grid size in variables

--- designs.js
+++ designs.js
@@ -5,6 +5,16 @@ $('#colorPicker').change(function() {
 	color = $('#colorPicker').val();
 });
 // Select size input
+let gridHeight = $('#input_height').val();
+let gridWidth = $('#input_width').val();
+
+$('#input_height').change(function() {
+	gridHeight = $('#input_height').val();
+});
+
+$('#input_width').change(function() {
+	gridWidth = $('#input_width').val();
+});
 
 // When size is submitted by the user, call makeGrid()
 
